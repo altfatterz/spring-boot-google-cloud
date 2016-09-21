239cad8093837c99527ac58c1d92fe094e986505
Zoltan Altfatter <altfatterz@gmail.com>
2016-09-21 13:25:52 +0200

    initial commit

---
+++ src/main/java/com/example/SpringBootGoogleCloudApplication.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringBootGoogleCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGoogleCloudApplication.class, args);
+	}
+
+	@RestController
+	class HelloWorldConroller {
+
+		@RequestMapping("/")
+		String greet() {
+			return "Hello World";
+		}
+	}
+}
