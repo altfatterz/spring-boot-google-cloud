3e9bf8dc8d25dd604639efdd83b006cea03cc3eb
Zoltan Altfatter <altfatterz@gmail.com>
2017-04-18 18:44:16 +0200

    fixes

--- src/main/java/com/example/SpringBootGoogleCloudApplication.java
+++ src/main/java/com/example/SpringBootGoogleCloudApplication.java
@@ -6,18 +6,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootGoogleCloudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootGoogleCloudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGoogleCloudApplication.class, args);
+    }
 
-	@RestController
-	class HelloWorldConroller {
 
-		@RequestMapping("/")
-		String greet() {
-			return "Hello World";
-		}
-	}
+    @RequestMapping("/")
+    String greet() {
+        return "Hello World";
+    }
+
+    /**
+     * <a href="https://cloud.google.com/appengine/docs/flexible/java/how-instances-are-managed#health_checking">
+     * App Engine health checking</a> requires responding with 200 to {@code /_ah/health}.
+     */
+    @RequestMapping("/_ah/health")
+    public String healthy() {
+        // Message body required though ignored
+        return "Still surviving.";
+    }
+
 }
